097d8243af1c1b0a68b7f0970d4110a00366b81d
Rezwan-Prince07 <demoprince610@gmail.com>
2025-09-07 22:24:44 +0600

    some changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import Banner from "@/components/Banner";
 import Faqs from "@/components/Faqs";
 
+
+
 import OurMission from "@/components/OurMission";
 import OurServices from "@/components/OurServices";
 
@@ -12,7 +14,14 @@ const Home = () => {
           <Banner/>
           <OurServices/>
           <OurMission/>
-          <Faqs/>
+         <Faqs
+          heading="FAQs about Parcel Delivery"
+        description="Find answers to common questions."
+        supportHeading="Still need help?"
+        supportDescription="Contact our support team for assistance."
+        supportButtonText="Contact Support"
+        supportButtonUrl="/support"
+         />
           {/* <OurTeam/> */}
           {/* <NewsLetter/> */}
         </div>
